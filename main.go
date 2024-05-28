9674452cec494d2c4a308949ea5416af7d23455a
Hasibul Hamid <hamidhasibul@gmail.com>
2024-05-29 01:02:07 +0600

    solved: 867

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	// arr := []int{1, 2, 3, 4}
 	// fmt.Println(sortedSquares(arr))
+
+	matrix := [][]int{{1, 2, 3}, {4, 5, 6}}
+
+	fmt.Println(tranpose(matrix))
+
 }
