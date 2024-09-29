16f116b3edf1e5505e1ad0c5c0f0400d76a6a000
Hasibul Hamid <hamidhasibul@gmail.com>
2024-09-29 13:34:57 +0600

    optimization: 125 using two pointers

--- 125-valid-palindrome.go
+++ 125-valid-palindrome.go
@@ -5,14 +5,36 @@ import (
 	"strings"
 )
 
+// func isPalindrome(s string) bool {
+// 	formattedStr := strings.ToLower(s)
+// 	formattedStr = regexp.MustCompile(`[^a-z0-9]+`).ReplaceAllString(formattedStr, "")
+// 	sir := []rune(formattedStr)
+
+// 	for i, j := 0, len(sir)-1; i < j; i, j = i+1, j-1 {
+// 		sir[i], sir[j] = sir[j], sir[i]
+// 	}
+
+// 	return formattedStr == string(sir)
+// }
+
 func isPalindrome(s string) bool {
+
 	formattedStr := strings.ToLower(s)
 	formattedStr = regexp.MustCompile(`[^a-z0-9]+`).ReplaceAllString(formattedStr, "")
-	sir := []rune(formattedStr)
 
-	for i, j := 0, len(sir)-1; i < j; i, j = i+1, j-1 {
-		sir[i], sir[j] = sir[j], sir[i]
+	str := []rune(formattedStr)
+
+	l := 0
+	r := len(str) - 1
+
+	for l < r {
+		if str[l] != str[r] {
+			return false
+		}
+
+		l++
+		r--
 	}
 
-	return formattedStr == string(sir)
+	return true
 }
--- main.go
+++ main.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Println(longestConsecutive([]int{100, 4, 200, 1, 3, 2}))
+	fmt.Println(isPalindrome(" "))
 }
